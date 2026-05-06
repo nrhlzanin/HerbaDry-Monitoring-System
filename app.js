95be09f6e5a99f8b1221cc4ed56ccb9ce1d77eaa
Nurhaliza Anindya Putri <144584107+nrhlzanin@users.noreply.github.com>
2026-05-06 20:56:41 +0700

    Fisrt Commit Website Monitoring Pengeringan

---
+++ app.js
@@ -0,0 +1,146 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+// ================= FIREBASE =================
+const firebaseConfig = {
+  apiKey: "AIzaSyAJs0emTj-c5kUSILGg9FR8YjoItVgG2MQ",
+  databaseURL: "https://iot-skripsi-ea976-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// ================= HEADER =================
+const wifiIcon = document.getElementById("wifiIcon");
+const espStatus = document.getElementById("espStatus");
+const clock = document.getElementById("clock");
+
+// ================= ELEMENT =================
+const suhu = document.getElementById("suhu");
+const kelembapan = document.getElementById("kelembapan");
+const soil = document.getElementById("soil");
+const status = document.getElementById("status");
+
+const notif = document.getElementById("notif");
+const buzzerStatus = document.getElementById("buzzerStatus");
+
+const popup = document.getElementById("popup");
+const popupText = document.getElementById("popupText");
+
+// ================= JAM =================
+setInterval(() => {
+  const now = new Date();
+
+  const h = String(now.getHours()).padStart(2, "0");
+  const m = String(now.getMinutes()).padStart(2, "0");
+  const s = String(now.getSeconds()).padStart(2, "0");
+
+  clock.innerText = `${h}:${m}:${s}`;
+}, 1000);
+
+// ================= STATUS ESP =================
+onValue(ref(db, "sensor/ip"), (snap) => {
+
+  if (snap.exists() && snap.val() !== "") {
+    wifiIcon.className = "wifi-icon wifi-online";
+    espStatus.innerText = "ESP32 Online";
+    espStatus.className = "esp-status esp-online";
+  } else {
+    wifiIcon.className = "wifi-icon wifi-offline";
+    espStatus.innerText = "ESP32 Offline";
+    espStatus.className = "esp-status esp-offline";
+  }
+
+});
+
+// ================= SENSOR =================
+onValue(ref(db, "sensor"), (snap) => {
+
+  const data = snap.val();
+  if (!data) return;
+
+  suhu.innerText = Number(data.suhu).toFixed(1) + " °C";
+  kelembapan.innerText = Number(data.kelembapan).toFixed(1) + " %";
+  soil.innerText = data.soil + " %";
+  status.innerText = data.kondisi;
+
+  // default popup hide
+  popup.classList.add("hidden");
+
+  // ================= STATUS BAR =================
+
+  if (data.kondisi === "Heating") {
+
+    notif.innerHTML =
+      '<i class="bi bi-fire"></i> Pemanasan Oven...';
+
+    notif.className = "notif aman";
+  }
+
+  else if (data.kondisi === "Optimal") {
+
+    notif.innerHTML =
+      '<i class="bi bi-check-circle-fill"></i> Suhu Optimal Tercapai';
+
+    notif.className = "notif aman";
+  }
+
+  else if (data.kondisi === "Warning") {
+
+    notif.innerHTML =
+      '<i class="bi bi-exclamation-triangle-fill"></i> Suhu Mulai Tinggi';
+
+    notif.className = "notif warning";
+
+    popup.classList.remove("hidden");
+    popupText.innerHTML = "WARNING!";
+  }
+
+  else {
+
+    notif.innerHTML =
+      '<i class="bi bi-exclamation-octagon-fill"></i> Suhu Sangat Tinggi!';
+
+    notif.className = "notif bahaya";
+
+    popup.classList.remove("hidden");
+    popupText.innerHTML = "DANGER!";
+  }
+
+  // jika moisture selesai
+  if (data.soil < 10) {
+    notif.innerHTML =
+      '<i class="bi bi-check2-square"></i> Pengeringan Selesai';
+    notif.className = "notif aman";
+  }
+
+});
+
+// ================= STATUS ALARM =================
+onValue(ref(db, "control/buzzerOff"), (snap) => {
+
+  if (snap.val() === true) {
+    buzzerStatus.innerText = "Alarm: OFF";
+  } else {
+    buzzerStatus.innerText = "Alarm: ON";
+  }
+
+});
+
+// ================= BUTTON =================
+window.matikanBuzzer = function () {
+  set(ref(db, "control/buzzerOff"), true);
+  popup.classList.add("hidden");
+};
+
+// ================= INTERNET USER =================
+setInterval(() => {
+  if (!navigator.onLine) {
+    wifiIcon.className = "wifi-icon wifi-offline";
+  }
+}, 3000);
